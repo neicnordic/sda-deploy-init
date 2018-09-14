b5f0937024b23eea434d23082197e27f578ba4c5
Stefan Negru <stefan@blankdots.com>
2018-09-14 10:18:44 +0300

    documentation updated with configuration generation information.

--- auto/deploy.py
+++ auto/deploy.py
@@ -4,8 +4,8 @@ import click
 import sys
 
 if sys.version_info[0] != 3 or sys.version_info[1] < 6:
-	print("This script requires Python version 3.6")
-	sys.exit(1)
+    print("This script requires Python version 3.6")
+    sys.exit(1)
 
 # Logging
 FORMAT = '[%(asctime)s][%(name)s][%(process)d %(processName)s][%(levelname)-8s] (L:%(lineno)s) %(funcName)s: %(message)s'
@@ -17,17 +17,15 @@ LOG.setLevel(logging.INFO)
 @click.command()
 @click.option('--config', is_flag=True,
               help='Flag for generating configuration if does not exist, or generating a new one.')
-@click.option('--config-path', help='Specify configuration directory.')
-@click.option('--deploy', is_flag=True,
-              help='Deploying the configuration secrets and pods.')
+@click.option('--config-path', help='Specify base path for the configuration directory.')
+@click.option('--deploy', is_flag=True, help='Deploying the configuration secrets and pods.')
 @click.option('--ns', default="testing", help='Deployment namespace, defaults to "testing".')
 @click.option('--cega-mq', default='cega-mq', help='CEGA MQ IP, for fake default "cega-mq".')
 @click.option('--cega-pwd', help='CEGA MQ Password, for fake CEGA MQ it is set up with a default.')
 @click.option('--cega-api', default='http://cega-users.testing:8001/user/',
               help='CEGA User endpoint, default http://cega-users.testing:8001/user/.')
 @click.option('--key-pass', default='password', help='CEGA Users RSA key password.')
-@click.option('--fake-cega', is_flag=True,
-              help='Deploy fake CEGA.')
+@click.option('--fake-cega', is_flag=True, help='Deploy fake CEGA.')
 def main(config, deploy, ns, config_path, cega_mq, cega_api, cega_pwd, key_pass, fake_cega):
     """Local EGA deployment script."""
     _localega = {
