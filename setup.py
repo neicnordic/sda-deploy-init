99e4397a9b2655f6cf58179b800fb49449431e80
Stefan Negru <stefan@blankdots.com>
2019-02-28 13:20:25 +0200

    make script a package

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lega_init',
+    version='0.1.0',
+    packages=find_packages(),
+    py_modules=['lega_init'],
+    include_package_data=True,
+    description='LocalEGA init script generating configuration parameters such as passwords and keys.',
+    author='LocalEGA Developers',
+    install_requires=[
+        'click>=6.7',
+        'PGPy==0.4.3',
+        'PyYAML',
+        'cryptography'
+    ],
+    entry_points={
+        'console_scripts': [
+            'legainit=lega_init.deploy:main'
+        ]
+    },
+)
