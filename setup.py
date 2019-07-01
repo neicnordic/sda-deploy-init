dedc76390513a69a3b78ac5186eaf74859d53992
Stefan Negru <stefan@blankdots.com>
2019-07-01 14:14:44 +0300

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lega_init',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     py_modules=['lega_init'],
     include_package_data=True,
