32c9bd5196921fecdbe8fa9972234dd1b28c3f1e
Stefan Negru <stefan@blankdots.com>
2019-12-04 01:11:07 +0200

    bump version to 0.3.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='legainit',
-    version='0.3.7',
+    version='0.3.8',
     packages=find_packages(),
     py_modules=['legainit'],
     include_package_data=True,
