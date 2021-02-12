1369dfa3215685b7ebbae4d00366cf34e993bb79
Stefan Negru <stefan@blankdots.com>
2021-02-12 16:48:35 +0200

    pin packages versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="legainit",
-    version="0.4.3",
+    version="0.4.4",
     packages=find_packages(),
     py_modules=["legainit"],
     include_package_data=True,
@@ -13,10 +13,11 @@ setup(
     author="LocalEGA Developers",
     package_data={"": ["*.sh"]},
     install_requires=[
-        "click",
-        "PGPy",
-        "ruamel.yaml",
-        "cryptography",
+        "click>=7.1.2",
+        "PGPy>=0.5.3",
+        "ruamel.yaml>=0.16.12",
+        "cryptography>=3.4.4",
+        "pyyaml~=5.3.1",
         "PyJWT>=1.7.1",
         "crypt4gh @ git+https://github.com/EGA-archive/crypt4gh.git@v1.1",
     ],
